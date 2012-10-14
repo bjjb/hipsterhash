e29bc842859218b9f29b20ea4d137cbaabec3b50
Bryan JJ Buckley <jjbuckley@gmail.com>
2012-10-14 16:11:46 +0200

    Docstring for the main class

--- lib/hipster_hash.rb
+++ lib/hipster_hash.rb
@@ -2,6 +2,9 @@
 require 'active_support/hash_with_indifferent_access'
 require 'active_support/core_ext/string/inflections'
 
+# A HipsterHash is like a regular Hash, except the keys are semi case
+# insensitive, and symbols and strings are equivalent. Therefore, :foo_bar,
+# :FooBar, and "foo_bar" are considered to be the same key.
 class HipsterHash < HashWithIndifferentAccess
   def [](key, *extras)
     super(convert_key(key), *extras)
